ec30ffbcab6fc91579ab09f07d192c9ff3743aca
Joshua Bellamy-Henn <josh@psidox.com>
2015-02-09 18:21:20 -0700

    Add back logging for production debug

--- backend/web/src/main/java/com/psidox/saddlewoof/web/rest/Interceptor.java
+++ backend/web/src/main/java/com/psidox/saddlewoof/web/rest/Interceptor.java
@@ -13,17 +13,17 @@ public class Interceptor implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext reqContext) throws IOException {
         
-//    	String request = "";
-//        request += String.format("uri: %s %s %s\n", reqContext.getMethod(), reqContext.getUriInfo().getPath(), reqContext.getUriInfo().getQueryParameters().toString());
-//    	request += String.format("lang: %s\n", reqContext.getAcceptableLanguages());
-//    	request += String.format("media: %s\n", reqContext.getAcceptableMediaTypes());
-//    	request += "headers:\n";
-//    	for (String key: reqContext.getHeaders().keySet()) {
-//    	   	request += String.format("+ %s: %s\n", key, reqContext.getHeaders().get(key));
-//    	}
-//		//request += String.format("body: %s\n", IOUtils.toString(reqContext.getEntityStream()));
-//
-//    	System.out.println(request);
+    	String request = "";
+        request += String.format("uri: %s %s %s\n", reqContext.getMethod(), reqContext.getUriInfo().getPath(), reqContext.getUriInfo().getQueryParameters().toString());
+    	request += String.format("lang: %s\n", reqContext.getAcceptableLanguages());
+    	request += String.format("media: %s\n", reqContext.getAcceptableMediaTypes());
+    	request += "headers:\n";
+    	for (String key: reqContext.getHeaders().keySet()) {
+    	   	request += String.format("+ %s: %s\n", key, reqContext.getHeaders().get(key));
+    	}
+		//request += String.format("body: %s\n", IOUtils.toString(reqContext.getEntityStream()));
+
+    	System.out.println(request);
     	
     }
 }
